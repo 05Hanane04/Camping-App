449488e7969ee1b9f66a7df576039dd5f758da8e
Hanane Challal <hananchallal07@gmail.com>
2026-07-07 16:42:50 +0100

    intern

--- src/Components/Accueil.js
+++ src/Components/Accueil.js
@@ -1,40 +1,33 @@
 import React, { useEffect, useState } from "react";
 import mainImg from "../images/mainImg.jpg";
-import camping from "../images/camping.jpg";
 import vuemer from "../images/vuemer.jpg";
-
 import a1 from "../images/a1.png";
 import a2 from "../images/a2.jpg";
 import a3 from "../images/a3.jpg";
 import s1 from "../images/s1.jpg";
 import s2 from "../images/s2.jpg";
 import arrow from "../images/arrow.png";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import AOS from "aos";
 import "aos/dist/aos.css";
-
 export default function Accueil() {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [isFading, setIsFading] = useState(false); 
-
-  const images = [mainImg, camping, vuemer];
-
+  const [isFading, setIsFading] = useState(false);
+  const images = [mainImg, vuemer];
   const changeImage = () => {
-    setIsFading(true); 
+    setIsFading(true);
     setTimeout(() => {
       setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-      setIsFading(false); 
-    }, 500); 
+      setIsFading(false);
+    }, 500);
   };
-
   useEffect(() => {
     AOS.init({ duration: 1000 });
 
     const intervalId = setInterval(changeImage, 3000);
     return () => clearInterval(intervalId);
   }, []);
-
-  return (
+  return(
     <div className="accueil-div">
       <div className="carousel">
         <img
@@ -44,60 +37,21 @@ export default function Accueil() {
           id="mainImg"
         />
       </div>
-
       <div className="titles">
         <h2 id="title1">Camping Terre d'Océan</h2>
         <h5 id="title2">Camping à Taghazout</h5>
       </div>
-
-      <div className="intro">
-        En savoir plus sur le Camping <img src={arrow} alt="" />
+      <div
+        className="intro"
+        onClick={() =>
+          document.querySelector(".presentation").scrollIntoView({
+            behavior: "smooth",
+          })
+        }
+      >
+        En savoir plus sur le Camping{" "}
+        <img src={arrow} alt="Flèche vers le bas" />
       </div>
-
-      <div className="accueil-imgs" data-aos="fade-up">
-        <img
-          src={a1}
-          alt="a1"
-          className="accueil-img"
-          data-aos="fade-up"
-          data-aos-delay="100"
-        />
-        <img
-          src={a2}
-          alt="a2"
-          className="accueil-img"
-          data-aos="fade-up"
-          data-aos-delay="300"
-        />
-        <img
-          src={a3}
-          alt="a3"
-          className="accueil-img"
-          data-aos="fade-up"
-          data-aos-delay="500"
-        />
-      </div>
-
-      <br /><br />
-
-      <div className="accueil-btn">
-        <button type="button" className="btn">
-          <Link className="nav-link" to="/hebergements">
-            Nos Hébergements
-          </Link>
-        </button>
-        <button type="button" className="btn">
-          <Link className="nav-link" to="/activites">
-            Notre Restaurant et sa piscine
-          </Link>
-        </button>
-        <button type="button" className="btn">
-          <Link className="nav-link" to="/contact">
-            Réservez maintenant !
-          </Link>
-        </button>
-      </div>
-
       <div className="presentation">
         <div className="text">
           <h2>Terre d'Océan - Camping Taghazout</h2>
@@ -127,7 +81,6 @@ export default function Accueil() {
           />
         </div>
       </div>
-
       <br />
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3435.4958142112537!2d-9.744307125815277!3d30.56354589379209!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xdb252926ae23c0d%3A0x2fa69f23de132268!2sCamping%20Terre%20d'Ocean!5e0!3m2!1sfr!2sma!4v1743066993444!5m2!1sfr!2sma"
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,29 +1,65 @@
-import React from 'react';
-import logo from '../images/logo.png';
+import React from "react";
+import logo from "../images/logo.png";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
-export default function Header(){
+export default function Header() {
+  const [scrolled, setScrolled] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 50);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
-    <div className='my-navbar'>
-        <nav className="navbar navbar-expand-lg">
-            <div className="container-fluid">
-            <Link className="nav-link" to="/"> <img src={logo} className="img-fluid" alt="Logo du Camping" /></Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <ul className="navbar-nav">
-                        <li className="nav-item"><Link className="nav-link" to="/">Accueil</Link></li>
-                        <li className="nav-item"><Link className="nav-link" to="/hebergements">Hébergements</Link></li>
-                        <li className="nav-item"><Link className="nav-link" to="/activites">Activités & Services</Link></li>
-                        <li className="nav-item"><Link className="nav-link" to="/contact">Contact & Réservation</Link></li>
-                        <li className="nav-item">
-                            <button type="button" id="btn" className="btn rounded-pill"><Link  id='btn-link' to="/login">Se connecter</Link></button>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+    <div className="my-navbar">
+      <nav
+        className={`navbar navbar-expand-lg my-navbar ${scrolled ? "scrolled" : ""}`}>
+        <div className="container-fluid">
+          <Link className="nav-link" to="/">
+            {" "}
+          <img src={logo} className="img-fluid" alt="Logo du Camping" />
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">
+                  Accueil
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/hebergements">
+                  Hébergements
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/activites">
+                  Activités & Services
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/contact">
+                  Contact & Réservation
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </div>
-);
+  );
 }
+
